79eb9a0be0aaf945938461a30c355ae4d6d363ac
tinaba96 <tinaba178.96@gmail.com>
2021-12-30 23:12:10 +0900

    env setup

--- react_native/App.js
+++ react_native/App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>リアルタイムに変更できる！w</Text>
+      <Text>面白い！w</Text>
       <StatusBar style="auto" />
     </View>
   );
